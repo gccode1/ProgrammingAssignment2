29e432187faac67f47cc16b42f7d9443c28236e4
Gaurav Singh <gaurav.s@minoty.com>
2017-09-22 11:43:28 +0530

    update

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,34 @@
+
+
+
+
+## This function make a cache of the matrix
 makeCacheMatrix <- function(x = matrix()) {
     inv = NULL
-    set <- function(y){
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    getInv <- function() inv
-    setInv <- function(y) inv <<- y
-    list(set = set, get = get,
+
+    # set function is not needed, I don't know why is it given in the assignment (while mean calculation)
+    # set <- function(y){
+    #     x <<- y
+    #     inv <<- NULL
+    # }
+
+    get <- function() x   # to get the matrix
+    getInv <- function() inv   # to get inverse of the matrix
+    setInv <- function(y) inv <<- y   # to set inverse of the matrix, where y is the inverse
+    list(get = get,          #  make a list of all the function, so that is can be accessed from outside
          setInv = setInv,
          getInv = getInv)
 }
 
 
 cacheSolve <- function(x, ...) {
-    inv <- x$getInv()
-    if(!is.null(inv)){
+    inv <- x$getInv()        # get inverse of the matrix
+    if(!is.null(inv)){       # if inv is not null, then return inverse from the cache
         message("getting cached data")
         return(inv)
     }
-    data = x$get()
-    inv = solve(data, ...)
-    x$setInv(inv)
-    inv
+    data = x$get() # get actual matrix from makeCacheMatrix object. 
+    inv = solve(data, ...)    # solve function will return the inverse of data
+    x$setInv(inv)             # set inverse so that we can get it from cache
+    inv                       # return inverse
 }
